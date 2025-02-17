55443d9429b8dcfae9a65940e6833509c91e9850
silvergjeka22 <86209628+silvergjeka22@users.noreply.github.com>
2025-02-17 20:41:29 +0100

    Refactoring

--- notebooks/helper/tools.py
+++ notebooks/helper/tools.py
@@ -1,33 +1,37 @@
+# helper/tools.py
+
 from sklearn.cluster import DBSCAN
 from sklearn.preprocessing import StandardScaler
 import numpy as np
 import pandas as pd
+from scipy.stats import normaltest
+
 
 class Tools:
     def __init__(self):
         pass
 
-    def tune_dbscan_and_interpolate(self, df, outliers_cols, eps_range=(0.1, 2.0, 0.1), min_samples_range=(5, 20)):
+    def tune_dbscan(self, df, outliers_cols, eps_range=(0.1, 2.0, 0.1), min_samples_range=(5, 20)):
         """
-        Function to tune DBSCAN parameters (eps and min_samples) for outlier detection.
+        Function to tune DBSCAN parameters (eps and min_samples) for outlier detection
+        and interpolate the outliers.
 
         Parameters:
-        - df: The DataFrame
+        - df: The DataFrame containing the sensor data
         - outliers_cols: List of column names to apply DBSCAN
         - eps_range: Range of values for the `eps` parameter (default: (0.1, 2.0, 0.1))
         - min_samples_range: Range of values for the `min_samples` parameter (default: (5, 20))
 
         Returns:
-        - best_eps: The best `eps`
-        - best_min_samples: The best `min_samples`
+        - best_eps: The best `eps` value found
+        - best_min_samples: The best `min_samples` value found
         - df_scaled: Scaled DataFrame
         """
         # Standard scale the data
         scaler = StandardScaler()
         df_scaled = df.copy()
         df_scaled[outliers_cols] = scaler.fit_transform(df[outliers_cols])
-
-
+        
         best_eps = None
         best_min_samples = None
         min_outliers = float('inf')
@@ -52,3 +56,19 @@ class Tools:
         print(f"Number of outliers with these parameters: {min_outliers}")
 
         return best_eps, best_min_samples, df_scaled
+
+    def check_normal_distribution(self, data, columns):
+        """
+        Function to check if columns of the DataFrame follow a normal distribution.
+        Parameters:
+        - data: The DataFrame
+        - columns: List of columns to check normality for
+        
+        Prints the result for each column.
+        """
+        for col in columns:
+            stat, p = normaltest(data[col])
+            if p > 0.05:
+                print(f'{col} is normally distributed (p-value: {p})')
+            else:
+                print(f'{col} is not normally distributed (p-value: {p})')
